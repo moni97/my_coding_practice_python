de6e6b9d81db7447b290107931198b02614cb6e7
moni97 <monisam97@gmail.com>
2022-02-16 19:46:29 -0600

    Create wordSearch.py

---
+++ WordSearch/wordSearch.py
@@ -0,0 +1,19 @@
+def exist(self, board: List[List[str]], word: str) -> bool:
+        row, col, word_len = len(board), len(board[0]), len(word)
+        def dfs(pos, index):
+            if index == word_len:
+                return True
+            if  pos[0] < 0 or pos[0] >= row or col <= pos[1] or pos[1] < 0 or index > word_len:
+                return False
+            if board[pos[0]][pos[1]] != word[index]:
+                return False
+            tmp = board[pos[0]][pos[1]]
+            board[pos[0]][pos[1]] = '#'
+            res = dfs((pos[0] + 1, pos[1]), index+1) or dfs((pos[0] - 1, pos[1]), index+1) or dfs((pos[0], pos[1] + 1), index + 1) or dfs((pos[0], pos[1] - 1), index + 1)
+            board[pos[0]][pos[1]] = tmp
+            return res     
+        for i in range(len(board)):
+            for j in range(len(board[i])):
+                if dfs((i, j), 0):
+                    return True
+        return False
