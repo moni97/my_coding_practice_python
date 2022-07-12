5b85b65e804a5b54a683bdd0ba76fdc0e01f5ae8
moni97 <monisam97@gmail.com>
2022-07-12 00:03:25 -0700

    Create binaryTreeRightSideView.py

---
+++ BinaryTreeRightSideView/binaryTreeRightSideView.py
@@ -0,0 +1,19 @@
+def rightSideView(self, root: Optional[TreeNode]) -> List[int]:
+        if root == None:
+            return []
+        from collections import deque
+        res = []
+        fifo = deque()
+        level = 1
+        dictCount = []
+        fifo.append((root, level))
+        while fifo:
+            for i in range(len(fifo)):
+                node, currLevel = fifo.popleft()
+                if node:
+                    if currLevel not in dictCount:
+                        res.append(node.val)
+                        dictCount.append(currLevel)
+                    fifo.append((node.right, currLevel + 1))
+                    fifo.append((node.left, currLevel + 1))
+        return res
