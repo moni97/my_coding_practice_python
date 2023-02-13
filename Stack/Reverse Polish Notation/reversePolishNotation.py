9c1442661dbbb945da385261e3fe9dfddebc4466
moni97 <monisam97@gmail.com>
2023-02-12 21:11:36 -0600

    Create reversePolishNotation.py

---
+++ Stack/Reverse Polish Notation/reversePolishNotation.py
@@ -0,0 +1,18 @@
+class Solution:
+    def evalRPN(self, tokens: List[str]) -> int:
+        stack = []
+        for t in tokens:
+            if t.lstrip('-+').isdigit():
+                stack.append(int(t))
+            else:
+                num1 = stack.pop()
+                num2 = stack.pop()
+                if t == "+":
+                    stack.append(num1+num2)
+                elif t == "-":
+                    stack.append(num2 - num1)
+                elif t == "/":
+                    stack.append(int(num2/num1))
+                elif t == "*":
+                    stack.append(num1 * num2)
+        return stack.pop()
