9a30762b3908aecfb8bfd0c5907606f85cb8e708
moni97 <monisam97@gmail.com>
2023-04-28 19:54:05 -0500

    Create findMiddleOfLinkedList.py

---
+++ Linked List/Find Middle of Linked List/findMiddleOfLinkedList.py
@@ -0,0 +1,12 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def middleNode(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        arr = []
+        while head:
+            arr.append(head)
+            head = head.next
+        return arr[len(arr) // 2]
